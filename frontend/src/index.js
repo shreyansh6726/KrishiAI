d22fd9a403f676fe02ab8f26aacb1d2e459dd12e
Shreyansh Srivastava <shreyansh.official.6726@gmail.com>
2026-02-11 17:57:50 +0530

    display message

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    ⚠️App under deverlopment 
+    ⚠️App under development 
   </React.StrictMode>
 );
